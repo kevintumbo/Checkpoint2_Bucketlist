9b0c70938d5b3fbe32cea1604c52ffb36c7d62e6
Kevin <kevin.tumbo39@gmail.com>
2017-05-26 16:27:42 +0300

    [chore] created config

---
+++ instance/config.py
@@ -0,0 +1,24 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    """Parent configuration class."""
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRET = os.getenv('SECRET')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////' + os.path.join(basedir, 'bucketlists.db')
+
+class DevelopmentConfig(Config):
+    """Configurations for Development."""
+    DEBUG = True
+
+class ProductionConfig(Config):
+    """Configurations for Production."""
+    DEBUG = False
+    TESTING = False
+
+app_config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+}
