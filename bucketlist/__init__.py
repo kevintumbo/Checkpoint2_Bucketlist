bfea3e6d34a5e51f72c35fe13b9c508a5a341865
Kevin <kevin.tumbo39@gmail.com>
2017-05-31 02:45:49 +0300

    [chore] add routes and blueprint

--- bucketlist/__init__.py
+++ bucketlist/__init__.py
@@ -1,33 +1,63 @@
-from flask_api import FlaskAPI
+from flask.ext.api import FlaskAPI, status, exceptions
 from flask_sqlalchemy import SQLAlchemy
-
+from flask import Blueprint, request, jsonify, abort
 # local import
 from instance.config import app_config
+from bucketlist import views
+
+# This instance of a Blueprint that represents the authentication blueprint
+auth_blueprint = Blueprint('auth', __name__)
 
 # initialize sql-alchemy
 db = SQLAlchemy()
 
 def create_app(config_name):
+    from bucketlist.models import Bucketlist
+
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config['development'])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
+    @app.route('/api/v1.0/bucketlists/', methods=['POST', 'GET'])
+    def bucketlists():
+        if request.method == "POST":
+            name = str(request.data.get('name', ''))
+            if name:
+                bucketlist = Bucketlist(name=name)
+                bucketlist.save()
+                response = jsonify({
+                    'id': bucketlist.id,
+                    'name': bucketlist.name,
+                    'date_created': bucketlist.date_created,
+                    'date_modified': bucketlist.date_modified
+                })
+                response.status_code = 201
+                return response
+        else:
+            # GET
+            bucketlists = Bucketlist.get_all()
+            results = []
+
+            for bucketlist in bucketlists:
+                obj = {
+                    'id': bucketlist.id,
+                    'name': bucketlist.name,
+                    'date_created': bucketlist.date_created,
+                    'date_modified': bucketlist.date_modified
+                }
+                results.append(obj)
+            response = jsonify(results)
+            response.status_code = 200
+            return response
+
+    @app.route('/bucketlists/<int:id>', methods=['POST','GET', 'PUT', 'DELETE'])
+    def bucketlist_manipulation(id, **kwargs):
+           #########################################################
+        ### Existing code for creating, updating and deleting a bucketlist #####
+        #########################################################
+
+        app.register_blueprint(auth_blueprint)
     return app
-
-# import os
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_restful import Api
-
-# # where the database will be stored
-# basedir = os.path.abspath(os.path.dirname(__file__))
-
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(basedir, 'bucketlists.db')
-# app.config.from_object('config')
-# db = SQLAlchemy(app)
-# api = Api(app)
-
 
--- bucketlist/models.py
+++ bucketlist/models.py
@@ -1,5 +1,8 @@
 import datetime
+from datetime import timedelta
 from bucketlist import db
+from flask_bcrypt import Bcrypt
+import jwt
 
 
 class User(db.Model):
@@ -15,7 +18,55 @@ class User(db.Model):
     bucketlists = db.relationship('Bucketlist', backref='users', lazy='dynamic')
     items = db.relationship('Item', backref='user', lazy='dynamic')
 
+    def __init__(self, username, email, password):
+        """Initialize the user with username, email and a password."""
+        self.username = username
+        self.email = email
+        self.password = Bcrypt().generate_password_hash(password).decode()
 
+    def password_is_valid(self, password):
+        """
+        Checks the password against it's hash to validates the user's password
+        """
+
+        return Bcrypt().check_password_hash(self.password, password)
+
+    def generate_token(self, id):
+        """ Generates the access token"""
+
+        try:
+            # set up a payload with an expiration time
+            payload = {
+                'exp': datetime.datetime.utcnow()+ timedelta(minutes=5),
+                'iat': datetime.datetime.utcnow(),
+                'sub': id
+            }
+            # create the byte string token using the payload and the SECRET key
+            jwt_string = jwt.encode(
+                payload,
+                'BLEED',
+                algorithm='HS256'
+            )
+            return jwt_string
+
+        except Exception as e:
+            # return an error in string format if an exception occurs
+            return str(e)
+
+    @staticmethod
+    def decode_token(token):
+        """Decodes the access token from the Authorization header."""
+        try:
+            # try to decode the token using our SECRET variable
+            payload = jwt.decode(token, 'SECRET')
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            # the token is expired, return an error string
+            return "Expired token. Please login to get a new token"
+        except jwt.InvalidTokenError:
+            # the token is invalid, return an error string
+            return "Invalid token. Please register or login"
+    
     def __repr__(self):
         return '<User %r>' % self.username
 
