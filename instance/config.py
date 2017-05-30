3ccbe54cfe04a18051308b9bae4732165d538170
Kevin <kevin.tumbo39@gmail.com>
2017-05-31 02:49:45 +0300

    [chore] edit config secret key

--- instance/config.py
+++ instance/config.py
@@ -6,7 +6,7 @@ class Config(object):
     """Parent configuration class."""
     DEBUG = False
     CSRF_ENABLED = True
-    SECRET = os.getenv('SECRET')
+    SECRET = os.getenv('BLEED')
     SQLALCHEMY_DATABASE_URI = 'sqlite:////' + os.path.join(basedir, 'bucketlists.db')
 
 class DevelopmentConfig(Config):
